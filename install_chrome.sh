bec144df4a096897b7d65a28668d2e5426ea6f2e
Cazeho <58745332+Cazeho@users.noreply.github.com>
2023-07-12 22:28:10 +0200

    Create install_chrome.sh

---
+++ install_chrome.sh
@@ -0,0 +1,6 @@
+apt update
+apt install -y wget curl unzip
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb
+apt --fix-broken install -y
+
